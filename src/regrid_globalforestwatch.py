9e8dcdbd241524c25e9c26cf228be64c5a999bca
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-07-06 16:19:43 +0100

    selecting subset of tiles to load in

--- src/regrid_globalforestwatch.py
+++ src/regrid_globalforestwatch.py
@@ -4,3 +4,29 @@
 # occurs in a specified timestep.  Initial estimates will be annual, but
 # subsequently will move onto monthly estimates using FORMA to distribute the
 # change through the year.
+
+# import required python libraries 
+import numpy as np
+import sys
+
+# import my own libraries
+import data_io as io
+import auxilliary_functions as aux
+
+# Where is the data?
+datadir = '/disk/scratch/local.2/dmilodow/GFW/'
+
+# where will we store data?
+savedir = '/disk/scratch/local.2/dmilodow/GFW/regridded/'
+
+# What is the bounding box of ROI? In this case use Mexico
+N = 33.
+S = 14.
+E = -118.
+W = -86.
+
+tile_list=np.genfromtxt(datadir+'tile_list.txt',dtype='string')
+n_tiles = len(tile_list)
+for i in range(0,n_tiles):
+    if io.is_GeoTIFF_within_bbox(datadir+tile_list[i],N,S,W,E):
+        print tile_list[i]
