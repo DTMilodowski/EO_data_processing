793b3c58931b925bc90fdb8408a055f3b9d60b7e
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-08-10 15:45:19 +0100

    new script to plot up maps of forest loss

---
+++ src/plot_EO_data/plot_forest_loss_maps.py
@@ -0,0 +1,3 @@
+# This function plots up a series of maps illustrating  forest loss through time,
+# as modelled based on global forest watch annual forest loss distributed temporally
+# based on the "seasonality" of FORMA 
