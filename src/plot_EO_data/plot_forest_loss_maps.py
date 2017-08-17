da8f93dd061ca9a89dca5f94aee7d06117cff024
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-08-17 12:05:15 +0100

    plotting arrays in basemap :-)

--- src/plot_EO_data/plot_forest_loss_maps.py
+++ src/plot_EO_data/plot_forest_loss_maps.py
@@ -3,7 +3,7 @@
 # based on the "seasonality" of FORMA 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
-from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.basemap import Basemap, shiftgrid
 import numpy as np
 
 # Some basic info for maps
@@ -12,22 +12,42 @@ S = 14.
 E = -86.
 W = -118.
 
+dY = 0.125
+dX = 0.125
+lat = np.arange(S,N,dY)#+dY/2. # shifting to cell centre
+lon = np.arange(W,E,dX)#+dX/2. # shifting to cell centre
+
+lon_grid,lat_grid = np.meshgrid(lon,lat)
+
+# Load in GFW
+regrid = np.load('regridded_data.npz')['arr_0']
+N_years=regrid.shape[-1]
+cum_regrid = np.cumsum(regrid,axis=2)
 
 # Now make the plots
-fig = plt.figure(1, facecolor='White',figsize=[5,8])
-ax1a= plt.subplot2grid((2,1),(0,0)) 
-ax1a.set_title('Monthly forest loss')
-m1a = Basemap(projection='aea', lat_0=(N+S)/2., lon_0=(E+W)/2., llcrnrlat=S, urcrnrlat=N,llcrnrlon=W, urcrnrlon=E, resolution='i')
-m1a.ax = ax1a
-m1a.drawcountries(color='0.6',linewidth=1)
-m1a.drawcoastlines(color='0.5',linewidth=1)
+for i in range(0,N_years):
+    fig = plt.figure(1, facecolor='White',figsize=[5,8])
+    ax1a= plt.subplot2grid((2,1),(0,0)) 
+    ax1a.set_title('Monthly forest loss')
+    m1a = Basemap(projection='aea', lat_0=(N+S)/2., lon_0=(E+W)/2., llcrnrlat=S, urcrnrlat=N,llcrnrlon=W, urcrnrlon=E, resolution='i')
+    m1a.ax = ax1a
+    x,y = m1a(lon_grid,lat_grid)
+    m1a.pcolormesh(x,y,regrid[:,:,i],vmin=0.0,vmax=0.2, cmap='jet', rasterized=False, edgecolor='0.6', linewidth=0)
+    #cbar = m1a.colorbar()
+    #cbar.solids.set_edgecolor("face")
+    #cbar.set_ticks([0,0.1,0.2])
+
+    m1a.drawcountries(color='0.6',linewidth=1)
+    m1a.drawcoastlines(color='0.5',linewidth=1)
 
 
-ax1b= plt.subplot2grid((2,1),(1,0)) 
-ax1b.set_title('Cumulative forest loss')
-m1b = Basemap(projection='aea', lat_0=(N+S)/2., lon_0=(E+W)/2., llcrnrlat=S, urcrnrlat=N,llcrnrlon=W, urcrnrlon=E, resolution='i')
-m1b.ax = ax1b
-m1b.drawcountries(color='0.6',linewidth=1)
-m1b.drawcoastlines(color='0.5',linewidth=1)
+    ax1b= plt.subplot2grid((2,1),(1,0)) 
+    ax1b.set_title('Cumulative forest loss')
+    m1b = Basemap(projection='aea', lat_0=(N+S)/2., lon_0=(E+W)/2., llcrnrlat=S, urcrnrlat=N,llcrnrlon=W, urcrnrlon=E, resolution='i')
+    m1b.ax = ax1b
+    x,y = m1b(lon_grid,lat_grid)
+    m1b.pcolormesh(x,y,cum_regrid[:,:,i],vmin=0.0,vmax=0.5, cmap='jet', rasterized=False, edgecolor='0.6', linewidth=0)
+    m1b.drawcountries(color='0.6',linewidth=1)
+    m1b.drawcoastlines(color='0.5',linewidth=1)
 
-plt.show()
+    plt.show()
