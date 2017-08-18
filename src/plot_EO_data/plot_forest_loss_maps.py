a911628a4cc7e6b0a39302eb0cfecfa052cbb654
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-08-18 14:09:23 +0100

    viridis colormap :-)

--- src/plot_EO_data/plot_forest_loss_maps.py
+++ src/plot_EO_data/plot_forest_loss_maps.py
@@ -6,6 +6,12 @@ from matplotlib.patches import Polygon
 from mpl_toolkits.basemap import Basemap, shiftgrid
 import numpy as np
 
+import sys
+sys.path.append('/home/dmilodow/DataStore_DTM/FOREST2020/EOdata/EO_data_processing/src/plot_EO_data/colormap/')
+import colormaps as cmaps
+plt.register_cmap(name='viridis', cmap=cmaps.viridis)
+plt.set_cmap(cmaps.viridis)
+
 # Some basic info for maps
 N = 33.
 S = 14.
@@ -34,10 +40,10 @@ for i in range(0,N_years):
     m1a = Basemap(projection='aea', lat_0=(N+S)/2., lon_0=(E+W)/2., llcrnrlat=S, urcrnrlat=N,llcrnrlon=W, urcrnrlon=E, resolution='i')
     m1a.ax = ax1a
     x,y = m1a(lon_grid,lat_grid)
-    m1a.pcolormesh(x,y,ForestLoss[i,:,:],vmin=0.0,vmax=0.2, cmap='jet', rasterized=True, edgecolor='0.6', linewidth=0)
-    #cbar = m1a.colorbar()
-    #cbar.solids.set_edgecolor("face")
-    #cbar.set_ticks([0,0.1,0.2])
+    im1 = m1a.pcolormesh(x,y,regrid[i,:,:],vmin=0.0,vmax=0.1, rasterized=True, edgecolor='0.6', linewidth=0)
+    cbar = m1a.colorbar(im1)
+    cbar.solids.set_edgecolor("face")
+    cbar.set_ticks([0,0.05,0.1])
 
     m1a.drawcountries(color='0.6',linewidth=1)
     m1a.drawcoastlines(color='0.5',linewidth=1)
@@ -48,8 +54,11 @@ for i in range(0,N_years):
     m1b = Basemap(projection='aea', lat_0=(N+S)/2., lon_0=(E+W)/2., llcrnrlat=S, urcrnrlat=N,llcrnrlon=W, urcrnrlon=E, resolution='i')
     m1b.ax = ax1b
     x,y = m1b(lon_grid,lat_grid)
-    m1b.pcolormesh(x,y,cum_regrid[i,:,:],vmin=0.0,vmax=0.5, cmap='jet', rasterized=True, edgecolor='0.6', linewidth=0)
+    im2=m1b.pcolormesh(x,y,cum_regrid[i,:,:],vmin=0.0,vmax=1, rasterized=True, edgecolor='0.6', linewidth=0)
     m1b.drawcountries(color='0.6',linewidth=1)
     m1b.drawcoastlines(color='0.5',linewidth=1)
+    cbar = m1b.colorbar(im2)
+    cbar.solids.set_edgecolor("face")
+    cbar.set_ticks([0,0.5,1])
 
-    plt.show()
+    plt.savefig('plot_EO_data/ForestLoss_tstep'+str(i).zfill(2)+'.png')
