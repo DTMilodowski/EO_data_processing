8ac8ac04969a961035bb1d8b00ebeb44048f9eda
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-09-22 14:11:32 +0100

    loop through timesteps

--- src/meteorology/load_ERAinterim.py
+++ src/meteorology/load_ERAinterim.py
@@ -12,4 +12,25 @@ import numpy as np
 from netCDF4 import Dataset
 
 #-------------------------------------------------------------------------------
-def load_ERAinterim_variable()
+# Script to load in daily ERA interim.
+# Variable names permitted are:
+# - mx2t
+# - mn2t
+# - t2m
+# - u10w (E-W)
+# - v10w (N-s)
+# - d2m
+# - psurf
+# - ssrd
+def load_ERAinterim_daily(path2files,variable,start_month,start_year,end_month,end_year):
+
+    month = np.arange(start_month,end_month+1)
+    year = np.arange(start_year,end_year+1)
+    tt = 0
+    for yy in range(0,year.size):
+        for mm in range(0,month.size):
+            NetCDF_file = path2files+'/'+variable+'_'+str(month[mm]).zfill(2)+str(year[yy])+'.nc'
+            dataset = Dataset(NetCDF_file)
+            tt+=1
+
+return met_array
