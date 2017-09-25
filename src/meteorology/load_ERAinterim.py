93ea58958944a545fc037b4615b81ccfdd6deb6a
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-09-25 12:37:36 +0100

    getting dates correct with gregorian conversion

--- src/meteorology/load_ERAinterim.py
+++ src/meteorology/load_ERAinterim.py
@@ -24,13 +24,33 @@ from netCDF4 import Dataset
 # - ssrd
 def load_ERAinterim_daily(path2files,variable,start_month,start_year,end_month,end_year):
 
-    month = np.arange(start_month,end_month+1)
+    # first of all find the first and last tile, and obtain the start and end
+    # date of the time series
+    # Note that ERA Interim times are in gregorian calendar format, hours after
+    # 1900-01-01 00:00
+    NetCDF_file = '%s/%s_%02i%04i.nc' % (path2files, variable,start_month,start_year)
+    dataset = Dataset(NetCDF_file)
+    start_greg = int(dataset.variables['time'][0])
+    start_date = (np.datetime64('1900-01-01 00:00') + np.timedelta64(start_greg,'h')).astype('datetime64[D]')
+
+    NetCDF_file = '%s/%s_%02i%04i.nc' % (path2files, variable,end_month,end_year)
+    dataset = Dataset(NetCDF_file)
+    end_greg = int(dataset.variables['time'][-1])
+    end_date = (np.datetime64('1900-01-01 00:00') + np.timedelta64(end_greg,'h')).astype('datetime64[D]')
+    
+    # create date list
     year = np.arange(start_year,end_year+1)
+    date = np.arange(start_date,end_date)
+    # create host array for met data    
+    eravar = np.zeros(date.size)*np.nan
+
     tt = 0
     for yy in range(0,year.size):
-        for mm in range(0,month.size):
-            NetCDF_file = path2files+'/'+variable+'_'+str(month[mm]).zfill(2)+str(year[yy])+'.nc'
-            dataset = Dataset(NetCDF_file)
-            tt+=1
+        for mm in range(0,12):
+            if tt < date.size:
+                NetCDF_file = '%s/%s_%02i%04i.nc' % (path2files, variable,month[mm],year[yy])
+                print NetCDF_file
+                dataset = Dataset(NetCDF_file)
+                tt+=1
 
-return met_array
+    return met_array
