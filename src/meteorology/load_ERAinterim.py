5ebb19c0e5551d7aa14b4b72729a5d1d65fdf43a
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-09-26 13:45:12 +0100

    fixing file name format

--- src/meteorology/load_ERAinterim.py
+++ src/meteorology/load_ERAinterim.py
@@ -188,12 +188,12 @@ def calculate_wind_speed_daily(path2files,start_month,start_year,end_month,end_y
     
     # Note that ERA Interim times are in gregorian calendar format, hours after
     # 1900-01-01 00:00
-    NetCDF_file = '%s/u10w_%02i%04i.nc' % (path2files,start_month,start_year)
+    NetCDF_file = '%s/u10w_%04i%02i.nc' % (path2files,start_year,start_month)
     dataset = Dataset(NetCDF_file)
     start_greg = int(dataset.variables['time'][0])
     start_date = (np.datetime64('1900-01-01 00:00') + np.timedelta64(start_greg,'h')).astype('datetime64[D]')
 
-    NetCDF_file = '%s/u10w_%02i%04i.nc' % (path2files,end_month,end_year)
+    NetCDF_file = '%s/u10w_%04i%02i.nc' % (path2files,end_year,end_month)
     dataset = Dataset(NetCDF_file)
     end_greg = int(dataset.variables['time'][-1])
     end_date = (np.datetime64('1900-01-01 00:00') + np.timedelta64(end_greg,'h')).astype('datetime64[D]')
@@ -208,8 +208,8 @@ def calculate_wind_speed_daily(path2files,start_month,start_year,end_month,end_y
     for yy in range(0,year.size):
         for mm in range(0,12):
             if tt < date.size:
-                u_file = '%s/u10w_%02i%04i.nc' % (path2files,month[mm],year[yy])
-                v_file = '%s/v10w_%02i%04i.nc' % (path2files,month[mm],year[yy])
+                u_file = '%s/u10w_%04i%02i.nc' % (path2files,year[yy],month[mm])
+                v_file = '%s/v10w_%04i%02i.nc' % (path2files,year[yy],month[mm])
                 # note that scale and offsets automatically applied when reading
                 # data in this way
                 ds_u = Dataset(u_file)
