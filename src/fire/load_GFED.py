ce4fc83330740e7a5bd3ce37e9b2468a37d14479
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-11-15 14:56:41 +0000

    paper references

--- src/fire/load_GFED.py
+++ src/fire/load_GFED.py
@@ -2,8 +2,16 @@
 # load_GFED.py
 #-------------------------------------------------------------------------------
 # @author D. T. Milodowski, November 2017
-# This is a set of functions to load in GFED burned area data into
+# This is a set of functions to load in GFED4 burned area data into
 # numpy arrays. These arrays have three dimensions: lat, long, time.
+#
+# Data references:
+# - Giglio, L., J.T. Randerson, and G.R. van der Werf, (2013), J. Geophys. Res. 
+#   Biogeosci., 118, 317328, doi:10.1002/jgrg.20042.
+# - Randerson, J.T., Y. Chen, G.R. van derWerf, B.M. Rogers, and D.C. Morton
+#   (2012), J. Geophys. Res., 117, G04012, doi:10.1029/2012JG002128.
+# - van der Werf, G.R., Randerson, J.T., Giglio, L., et al., (2017), Earth Syst.. 
+#   Sci. Data, 9, 697-720, https://doi.org/10.5194/essd-9-697-2017.
 #===============================================================================
 # import standard libraries
 import numpy as np
