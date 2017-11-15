c617fdc9ab50fed91f8ba4ebc0edb185a0b14920
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-11-15 16:07:26 +0000

    defining time series£

--- src/fire/load_GFED.py
+++ src/fire/load_GFED.py
@@ -2,8 +2,8 @@
 # load_GFED.py
 #-------------------------------------------------------------------------------
 # @author D. T. Milodowski, November 2017
-# This is a set of functions to load in GFED4 burned area data into
-# numpy arrays. These arrays have three dimensions: lat, long, time.
+# This is a set of functions to load in GFED4 burned area data into numpy arrays 
+# These arrays have three dimensions: lat, long, time, with a monthly timestep
 #
 # Data references:
 # - Giglio, L., J.T. Randerson, and G.R. van der Werf, (2013), J. Geophys. Res. 
@@ -18,4 +18,14 @@ import numpy as np
 
 # import netcdf libraries
 from netCDF4 import Dataset
+
+# Function to load GFED4 monthly data. There are two potential variables here:
+# - burned area expressed as fraction of a pixel: BurnedFraction
+# - burned area expressed as the area in square metres: BurnedArea
+# Files collate annual data, with timestep indicated by the day of the year.
+def load_GFED4_monthly(path2files,variable,start_month,start_year,end_month,end_year):
+
+    # first of all obtain the start and end date of the time series
+    start_date = np.datetime64('%04i-%02i' % (start_year,start_month))
+    end_date = (np.datetime64('%04i-%02i' % (end_year,end_month))+np.timedelta64(1,'M'))
 
